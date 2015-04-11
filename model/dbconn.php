2f7d532e025279ed1167a0d502a5ecdc661555b7
czhoume <czhoume@gmail.com>
2015-04-11 15:00:26 -0700

    print yay!

--- model/dbconn.php
+++ model/dbconn.php
@@ -10,7 +10,7 @@ $conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db
 if($conn === false){
     die(print_r(sqlsrv_errors()));
 }else{
-	print_r("yay!");
+	echo "yay!";
 }
 
 ?>
