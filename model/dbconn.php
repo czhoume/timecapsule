28a310322890db9d8783d56f03908459d87ffc85
czhoume <czhoume@gmail.com>
2015-04-11 14:56:39 -0700

    test db

---
+++ model/dbconn.php
@@ -0,0 +1,12 @@
+$server = "tcp:rjv15kvxeq.database.windows.net";
+$user = "capsuletime"@SERVER_ID;
+$pwd = "Password1";
+$db = "rjv15kvxeq";
+
+$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
+
+if($conn === false){
+    die(print_r(sqlsrv_errors()));
+}else{
+	print_r("yay!");
+}
