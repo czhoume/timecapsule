ac73cf422d84295391f8db981c2f626c6384dc71
czhoume <czhoume@gmail.com>
2015-04-11 18:47:16 -0700

    try another sample code

--- model/dbconn.php
+++ model/dbconn.php
@@ -4,20 +4,14 @@
 <?php
 echo "is it working?";
 phpinfo();
-$server = "tcp:rjv15kvxeq.database.windows.net,1433";
-$user = "capsuletime"@SERVER_ID;
-$pwd = "Password1";
-$db = "rjv15kvxeq";
-
-$conn = sqlsrv_connect($server, array("UID"=>$user, "PWD"=>$pwd, "Database"=>$db));
-
-if($conn === false){
-	echo "fail";
-    //die(print_r(sqlsrv_errors()));
-}else{
-	echo "yay!";
+try {
+   $conn = new PDO ( "sqlsrv:server = tcp:rjv15kvxeq.database.windows.net,1433; Database = capsuletime", "capsuletime", "Password1");
+   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
-
+catch ( PDOException $e ) {
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+}
 ?>
 
 </body>
