faa14e404df2f6825e4fd7561c83fde4d5459b15
czhoume <czhoume@gmail.com>
2015-04-11 18:53:34 -0700

    this ui is slow

--- model/dbconn.php
+++ model/dbconn.php
@@ -6,6 +6,7 @@ echo "is it working?";
 try {
    $conn = new PDO ( "sqlsrv:server = tcp:rjv15kvxeq.database.windows.net,1433; Database = capsuletime", "capsuletime", "Password1");
    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+   echo "looks like worked";
 }
 catch ( PDOException $e ) {
    print( "Error connecting to SQL Server." );
