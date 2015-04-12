4202de7ff0d1c33ec89e14da672d9f4c2f96a4fc
czhoume <czhoume@gmail.com>
2015-04-11 18:51:13 -0700

    hmm

--- model/dbconn.php
+++ model/dbconn.php
@@ -4,7 +4,14 @@
 <?php
 echo "is it working?";
 phpinfo();
-
+try {
+   $conn = new PDO ( "sqlsrv:server = tcp:rjv15kvxeq.database.windows.net,1433; Database = capsuletime", "capsuletime", "Password1");
+   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) {
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+}
 ?>
 
 </body>
