b3db3d168e548cc2ba301c755b3ec6ef5318551b
czhoume <czhoume@gmail.com>
2015-04-15 16:40:21 -0700

    create

--- controller/dbtest.php
+++ controller/dbtest.php
@@ -3,22 +3,46 @@
 require_once '..\vendor\autoload.php';
 
 use WindowsAzure\Common\ServicesBuilder;
+use WindowsAzure\Blob\Models\CreateContainerOptions;
+use WindowsAzure\Blob\Models\PublicAccessType;
 use WindowsAzure\Common\ServiceException;
 
 $connectionString="DefaultEndpointsProtocol=https;AccountName=timecapsule;AccountKey=Svqu96cufSCabYoUWYs/2z38APOyEYftwJqS5ccFkLKpIQ+QPVhEYRoQt6mdcDbZ4Gj5b0elCcwRsEqc1a8peg==";
+
 
 
 // Create blob REST proxy.
 $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
 
 
-$content = fopen("C:\tmpdownload\20150414ExtensionList.txt", "r");
-$blob_name = "myblob";
+// OPTIONAL: Set public access policy and metadata.
+// Create container options object.
+$createContainerOptions = new CreateContainerOptions(); 
+
+// Set public access policy. Possible values are 
+// PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+// CONTAINER_AND_BLOBS:     
+// Specifies full public read access for container and blob data.
+// proxys can enumerate blobs within the container via anonymous 
+// request, but cannot enumerate containers within the storage account.
+//
+// BLOBS_ONLY:
+// Specifies public read access for blobs. Blob data within this 
+// container can be read via anonymous request, but container data is not 
+// available. proxys cannot enumerate blobs within the container via 
+// anonymous request.
+// If this value is not specified in the request, container data is 
+// private to the account owner.
+$createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+// Set container metadata
+$createContainerOptions->addMetaData("key1", "value1");
+$createContainerOptions->addMetaData("key2", "value2");
 
 try {
-    //Upload blob
-    $blobRestProxy->createBlockBlob("mycontainer", $blob_name, $content);
-	echo "uploaded";
+    // Create container.
+    $blobRestProxy->createContainer("mycontainer1", $createContainerOptions);
+	echo "created";
 }
 catch(ServiceException $e){
     // Handle exception based on error codes and messages.
