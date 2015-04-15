9d314bf99813bc9edd7a5270d276cd57c10ea71f
czhoume <czhoume@gmail.com>
2015-04-15 16:37:17 -0700

    keep revert

--- controller/dbtest.php
+++ controller/dbtest.php
@@ -5,19 +5,20 @@ require_once '..\vendor\autoload.php';
 use WindowsAzure\Common\ServicesBuilder;
 use WindowsAzure\Common\ServiceException;
 
-$connectionString="DefaultEndpointsProtocol=https;AccountName=timecapsule;AccountKey=Svqu96cufSCabYoUWYs/2z38APOyEYftwJqS5ccFkLKpIQ+QPVhEYRoQt6mdcDbZ4Gj5b0elCcwRsEqc1a8peg==;";
+$connectionString="DefaultEndpointsProtocol=https;AccountName=timecapsule;AccountKey=Svqu96cufSCabYoUWYs/2z38APOyEYftwJqS5ccFkLKpIQ+QPVhEYRoQt6mdcDbZ4Gj5b0elCcwRsEqc1a8peg==";
 
 
 // Create blob REST proxy.
 $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
 
 
-$content = fopen("c:\tmpdownload\20150414ExtensionList.txt", "r");
+$content = fopen("C:\tmpdownload\20150414ExtensionList.txt", "r");
 $blob_name = "myblob";
 
 try {
-    // Create container.
-    $blobRestProxy->createContainer("mycontainer", $createContainerOptions);
+    //Upload blob
+    $blobRestProxy->createBlockBlob("mycontainer", $blob_name, $content);
+	echo "uploaded";
 }
 catch(ServiceException $e){
     // Handle exception based on error codes and messages.
@@ -27,5 +28,4 @@ catch(ServiceException $e){
     $error_message = $e->getMessage();
     echo $code.": ".$error_message."<br />";
 }
-
 ?>
