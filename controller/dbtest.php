48affcc99faa22d9f5bd4338a113bf074b52e3f5
czhoume <czhoume@gmail.com>
2015-04-16 09:27:03 -0700

    testing

--- controller/dbtest.php
+++ controller/dbtest.php
@@ -9,42 +9,5 @@ use WindowsAzure\Common\ServiceException;
 
 $connectionString="DefaultEndpointsProtocol=https;AccountName=timecapsule;AccountKey=Svqu96cufSCabYoUWYs/2z38APOyEYftwJqS5ccFkLKpIQ+QPVhEYRoQt6mdcDbZ4Gj5b0elCcwRsEqc1a8peg==";
 
-
-// OPTIONAL: Set public access policy and metadata.
-// Create container options object.
-$createContainerOptions = new CreateContainerOptions(); 
-
-// Set public access policy. Possible values are 
-// PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
-// CONTAINER_AND_BLOBS:     
-// Specifies full public read access for container and blob data.
-// proxys can enumerate blobs within the container via anonymous 
-// request, but cannot enumerate containers within the storage account.
-//
-// BLOBS_ONLY:
-// Specifies public read access for blobs. Blob data within this 
-// container can be read via anonymous request, but container data is not 
-// available. proxys cannot enumerate blobs within the container via 
-// anonymous request.
-// If this value is not specified in the request, container data is 
-// private to the account owner.
-$createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-
-// Set container metadata
-$createContainerOptions->addMetaData("key1", "value1");
-$createContainerOptions->addMetaData("key2", "value2");
-
-try {
-    // Create container.
-    $blobRestProxy->createContainer("mycontainer2", $createContainerOptions);
-    echo"created";
-}
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here: 
-    // http://msdn.microsoft.com/library/azure/dd179439.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
-}
+echo "yay!";
 ?>
