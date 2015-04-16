e97eb6cf1bb66a51c03a45e74dd6d91392043df5
czhoume <czhoume@gmail.com>
2015-04-16 10:21:25 -0700

    111

--- controller/dbtest.php
+++ controller/dbtest.php
@@ -14,6 +14,7 @@ $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionSt
 try{
 	$content = fopen("c:\\hacktest\\testtxt.txt", "r");
 }catch(Exception $e){
+	echo "sth";
 	echo $e->getMessage();
 }
 
