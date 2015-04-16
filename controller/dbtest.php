20840b718ecaef5bdff9a2298275b0d839b31041
czhoume <czhoume@gmail.com>
2015-04-16 09:39:26 -0700

    try http

--- controller/dbtest.php
+++ controller/dbtest.php
@@ -7,7 +7,7 @@ use WindowsAzure\Blob\Models\CreateContainerOptions;
 use WindowsAzure\Blob\Models\PublicAccessType;
 use WindowsAzure\Common\ServiceException;
 
-$connectionString="DefaultEndpointsProtocol=https;AccountName=timecapsule;AccountKey=Svqu96cufSCabYoUWYs/2z38APOyEYftwJqS5ccFkLKpIQ+QPVhEYRoQt6mdcDbZ4Gj5b0elCcwRsEqc1a8peg==";
+$connectionString="DefaultEndpointsProtocol=http;AccountName=timecapsule;AccountKey=Svqu96cufSCabYoUWYs/2z38APOyEYftwJqS5ccFkLKpIQ+QPVhEYRoQt6mdcDbZ4Gj5b0elCcwRsEqc1a8peg==";
 
 
 
